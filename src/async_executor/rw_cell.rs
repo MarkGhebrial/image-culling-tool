7a1443702914e27de27ae0e756e4060a57ec96dd
MarkGhebrial <mghebrialjr@icloud.com>
2026-07-11 16:21:37 -0700

    Remove commented out code from rw_cell.rs

--- src/async_executor/rw_cell.rs
+++ src/async_executor/rw_cell.rs
@@ -10,49 +10,6 @@ unsafe fn my_alloc<T>() -> NonNull<T> {
     NonNull::new(raw_ptr).unwrap()
 }
 
-// #[derive(Clone)]
-// struct OneInner {
-//     /// Indicates if either of the two owners have been dropped.
-//     other_was_dropped: NonNull<AtomicBool>,
-// }
-// impl OneInner {
-//     fn new() -> Self {
-//         let layout = Layout::new::<AtomicBool>();
-//         let owner_was_dropped = unsafe {
-//             let raw_ptr = alloc(layout) as *mut AtomicBool;
-//             let mut ptr = NonNull::new(raw_ptr).unwrap();
-//             *ptr.as_mut() = AtomicBool::new(false);
-//             ptr
-//         };
-
-//         Self { owner_was_dropped }
-//     }
-// }
-
-// impl Drop for OneInner {
-//     fn drop(&mut self) {
-//         // If the inner value has already been dropped once, that means that this is the second drop
-//         // and we must deallocate the inner value
-//         unsafe {
-//             // If the value has already been dropped by the OneCell
-//             if self.owner_was_dropped.as_ref().load(Ordering::SeqCst) {
-//                 println!("Second inner drop (deallocating the atomic bool)");
-//                 dealloc(
-//                     self.owner_was_dropped.as_ptr() as *mut u8,
-//                     Layout::new::<AtomicBool>(),
-//                 );
-//             }
-//             // This is the first drop of the inner value, so indicate to the OneCell that it should take care of the deallocation
-//             else {
-//                 println!("First inner drop (storing true into the atomic bool)");
-//                 self.owner_was_dropped
-//                     .as_mut()
-//                     .store(true, Ordering::SeqCst);
-//             }
-//         }
-//     }
-// }
-
 struct TwoCellInner<T> {
     value: T,
 
@@ -113,54 +70,6 @@ impl<T> TwoCell<T> {
     }
 }
 
-// struct WeakCell<T> {
-//     /// It may be non-null, but it may be dangling!
-//     value_ptr: NonNull<T>,
-
-//     // The pointer is not null, but it may point to freed memory!
-//     inner: OneInner,
-// }
-// impl<T> WeakCell<T> {
-//     pub fn is_owner_alive(&self) -> bool {
-//         unsafe { !self.inner.owner_was_dropped.as_ref().load(Ordering::SeqCst) }
-//     }
-
-//     pub fn promote<'a>(&'a self) -> Option<PromotedWeakCell<T>> {
-//         if self.is_owner_alive() {
-//             unsafe {
-//                 Some(PromotedWeakCell {
-//                     value_ref: self.value_ptr.as_ref(),
-//                     inner: self.inner.clone(),
-//                 })
-//             }
-//         } else {
-//             None
-//         }
-//     }
-
-//     pub fn get(&self) -> Option<&T> {
-//         if self.is_owner_alive() {
-//             // This is unsound if the OwnedCell deallocates the value while this reference is held! BIG NO NO!!! DO NOT DO THIS!!!
-//             unsafe { Some(self.value_ptr.as_ref()) }
-//         } else {
-//             None
-//         }
-//     }
-// }
-
-// struct PromotedWeakCell<'a, T> {
-//     value_ref: &'a T,
-//     inner: OneInner,
-// }
-// impl<'a, T> Deref for PromotedWeakCell<'a, T> {
-//     type Target = T;
-
-//     fn deref(&self) -> &Self::Target {
-//         self.value_ref
-//     }
-// }
-
-// unsafe impl<T> Send for WeakCell<T> {}
 unsafe impl<T> Send for TwoCell<T> {}
 
 #[test]
