510537444bc9b2f2ccd36f4d08976d0f4dc5f003
MarkGhebrial <mghebrialjr@icloud.com>
2026-07-22 20:54:19 -0700

    Keep cursor over same image point during zoom
    I hacked this together with minimal pen and paper, so everything is
    still spaghetti. Commiting now while it works.

--- src/zoom_image_widget.rs
+++ src/zoom_image_widget.rs
@@ -2,7 +2,8 @@ use std::sync::Arc;
 
 use eframe::{
     egui::{
-        self, Color32, ImageData, Mesh, Pos2, Rect, Sense, TextureId, TextureOptions, Vec2, Widget,
+        self, Color32, ImageData, Mesh, Pos2, Rect, Sense, TextureOptions, Vec2, Widget,
+        load::SizedTexture,
     },
     epaint::ImageDelta,
 };
@@ -10,19 +11,25 @@ use eframe::{
 use crate::util::rect_with_aspect_ratio;
 
 pub struct ZoomImage {
-    texture_id: TextureId,
-    // The width and height of the image in pixels
-    image: Arc<dyn ImageData>,
+    sized_texture: SizedTexture,
 
     /// The point in the image to zoom in to. From (0.0, 0.0) to (1.0, 1.0)
     zoom_translation: Vec2,
 
-    /// The amount that the image is zoomed in. 0.0 means "fit the image to the\
-    /// bounding rect" and 1.0 means "idk bro, just zoom the image in all the way"
+    /// The amount that the image is zoomed relative to its size when all the way
+    /// zoomed out. Anything less than 1.0 is invalid
     zoom_scale: f32,
 }
 
 impl ZoomImage {
+    // pub fn new(sized_texture: SizedTexture) -> Self {
+    //     Self {
+    //         sized_texture,
+    //         zoom_translation: Vec2::ZERO,
+    //         zoom_scale: 0.0,
+    //     }
+    // }
+
     pub fn new(image: Arc<impl ImageData>, ctx: &egui::Context) -> Self {
         let texture_id = ctx.tex_manager().write().alloc(
             "zoom image".to_owned(),
@@ -31,19 +38,25 @@ impl ZoomImage {
         );
 
         Self {
-            texture_id,
-            image,
-            zoom_translation: Vec2::new(0.0, 0.0),
+            sized_texture: SizedTexture::new(
+                texture_id,
+                [image.width() as f32, image.height() as f32],
+            ),
+            zoom_translation: Vec2::ZERO,
             zoom_scale: 1.0,
         }
     }
 
+    // pub fn set_texture(&mut self, sized_texture: SizedTexture) {
+    //     self.sized_texture = sized_texture;
+    // }
+
     pub fn set_image(&mut self, image: Arc<impl ImageData>, ctx: &egui::Context) {
         ctx.tex_manager().write().set(
-            self.texture_id,
+            self.sized_texture.id,
             ImageDelta::full(image.clone(), TextureOptions::default()),
         );
-        self.image = image;
+        self.sized_texture.size = [image.width() as f32, image.height() as f32].into();
     }
 
     pub fn reset_zoom(&mut self) {
@@ -57,61 +70,84 @@ impl Widget for &mut ZoomImage {
         // The rect is the bounding box we have to work with. Maximally zoomed out,
         // two edges of the image align with two edges of the rect, and the other
         // two pairs of edges are equally distant from each other.
-        let (rect, response) = ui.allocate_exact_size(ui.available_size(), Sense::all());
+        let (rect, response) = ui.allocate_exact_size(ui.available_size(), Sense::empty());
 
-        // width/height
-        // Larger aspect ratio -> wider, shorter
-        // Smaller aspect ratio -> taller, skinnier
-        // let rect_aspect_ratio = rect.aspect_ratio();
-        let img_aspect_ratio = self.image.size()[0] as f32 / self.image.size()[1] as f32;
+        let image_size = self.sized_texture.size;
+        let img_aspect_ratio = image_size.x / image_size.y;
 
-        let image_rect = rect_with_aspect_ratio(&rect, img_aspect_ratio)
+        let mut image_rect = rect_with_aspect_ratio(&rect, img_aspect_ratio)
             .translate(self.zoom_translation)
             .scale_from_center(self.zoom_scale);
 
-        let image_rect_response = ui.allocate_rect(image_rect, Sense::all());
+        let image_rect_response = ui.allocate_rect(image_rect, Sense::HOVER | Sense::DRAG);
+
+        // Adjust the image translation based on the drag amount
+        let drag_amount = image_rect_response.drag_delta();
 
         // If the image is being hovered
-        if let Some(_hover_pos) = image_rect_response.hover_pos() {
+        if let Some(hover_pos) = image_rect_response.hover_pos() {
             // Get access to the egui InputState
             ui.input_mut(|input| {
                 // TODO: Use this to calculate the % zoom factor
                 input.physical_pixel_size();
 
-                let drag_amount =
-                    if input.pointer.is_decidedly_dragging() && input.pointer.primary_down() {
-                        let ptr = &input.pointer;
-                        ptr.delta()
-                    } else {
-                        Vec2::ZERO
-                    };
-
                 let Vec2 {
                     y: y_scroll,
                     x: x_scroll,
                 } = input.smooth_scroll_delta;
-                // Reset the scroll amount so that any scroll containers don't also scroll in this frame
-                input.raw_scroll_delta = Vec2::ZERO;
+
+                // Adjust the image zoom based on the scroll amount
+                let mut new_zoom_scale = self.zoom_scale + (y_scroll * 0.05 * (self.zoom_scale));
+                new_zoom_scale *= input.zoom_delta(); // Handle touchscreen zooms
+                if new_zoom_scale < 1.0 {
+                    new_zoom_scale = 1.0;
+                }
+
+                let zoom_scale_delta = new_zoom_scale - self.zoom_scale;
+
+                // // Adjust the image zoom based on the scroll amount
+                // self.zoom_scale += y_scroll * 0.05 * (self.zoom_scale);
+                // self.zoom_scale *= input.zoom_delta(); // Handle touchscreen zooms
+                // if self.zoom_scale < 1.0 {
+                //     self.zoom_scale = 1.0;
+                // }
+
+                let new_image_rect = rect_with_aspect_ratio(&rect, img_aspect_ratio)
+                    .translate(self.zoom_translation)
+                    .scale_from_center(new_zoom_scale);
+
+                let relative_point_1 = (hover_pos - image_rect.center()) / image_rect.size();
+                let relative_point_2 =
+                    (hover_pos - new_image_rect.center()) / new_image_rect.size();
+
+                // We need to translate the new image rect so that relative point 2 moves exactly on top of relative point one
+                let xlation = (relative_point_2 - relative_point_1) * new_image_rect.size();
+
+                self.zoom_translation += xlation;
+                self.zoom_translation += drag_amount;
+
+                println!(
+                    "xlation = {:?}; rps: {:?}, {:?}",
+                    xlation, relative_point_1, relative_point_2
+                );
+
+                // It's the difference between image rect center and cursor position, times [image width, image height]
+                // let stay_centered_on_cursor_offset =
+                // (hover_pos - image_rect.center()) * image_rect.size() * zoom_scale_delta;
+                // self.zoom_translation += ;
+
+                self.zoom_scale = new_zoom_scale;
 
                 println!("Touch zoom {}", input.zoom_delta());
                 println!("Scroll: ({}, {})", x_scroll, y_scroll);
-
-                // let normalized_hover_pos = hover_pos.to_vec2() / rect.size();
-
-                // Adjust the image translation based on the drag amount
-                self.zoom_translation += drag_amount;
-
-                // Adjust the image zoom based on the scroll amount
-                self.zoom_scale += y_scroll * 0.05 * (self.zoom_scale);
-                self.zoom_scale *= input.zoom_delta(); // Handle touchscreen zooms
-                if self.zoom_scale < 1.0 {
-                    self.zoom_scale = 1.0;
-                }
             });
         }
 
-        if ui.is_rect_visible(rect) {
-            let mut mesh = Mesh::with_texture(self.texture_id);
+        // image_rect = image_rect.translate(self.zoom_translation)
+        //     .scale_from_center(self.zoom_scale);
+
+        if ui.is_rect_visible(image_rect) {
+            let mut mesh = Mesh::with_texture(self.sized_texture.id);
             mesh.add_rect_with_uv(
                 image_rect, //rect,
                 Rect::from_min_max(Pos2::new(0.0, 0.0), Pos2::new(1.0, 1.0)),
